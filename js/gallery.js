8cd3646aa04d96f9e07e09918321ce2528a06017
HaThucQuocHung-FPT <htquochung.dev@gmail.com>
2025-06-17 20:38:14 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,3 @@
-// function upDate(previewPic) {
-//   document.getElementById("image").innerHTML = previewPic.alt;
-//   document.getElementById("image").style.backgroundImage =
-//     "url(" + previewPic.src + ")";
-// }
-
-// function unDo() {
-//   document.getElementById("image").style.backgroundImage = "url('')";
-//   document.getElementById("image").innerHTML =
-//     "Hover over an image below to display here.";
-// }
 window.addEventListener("DOMContentLoaded", function () {
   const previewImages = document.querySelectorAll(".preview");
   const displayBox = document.getElementById("image");
